4fb29f31b142b12666273f51f21c2b7da906659b
decodewithParth <parthc2002@gmail.com>
2024-01-24 15:35:28 +0530

    Vodeo - 7 : learned about the operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,5 +26,43 @@ let booleanIsLoggedIn=Boolean(isLoggedIn);
 let someNumber=42
 
 let stringNumber=String(someNumber);
-console.log(stringNumber)
-console.log(typeof(stringNumber))
+// console.log(stringNumber)
+// console.log(typeof(stringNumber))
+
+
+// ************************* Operations ***********************
+
+let value=9
+let negValue=-value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1=" PArth";
+let str2="Hello"
+
+let str3=str2+str1;
+// console.log(str3);
+
+// console.log(1+"2");
+// console.log('1'+2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+
+let num1,num2,num3;
+num1=num2=num3=2+2;
+
+let gameCounter=100;
+gameCounter++;
+console.log(++gameCounter);
