f02f5bc068f55d446ea81326fd5e6de8828b8927
decodewithParth <parthc2002@gmail.com>
2024-01-24 16:09:02 +0530

    Video - 9 : Datatypes summary

---
+++ dataTypesSummary.js
@@ -0,0 +1,36 @@
+//  Primitive - also called call by value type
+
+// 7 types : String, Number , Boolean, Null, Undefined, Symbol, BigInt
+
+// examples:
+
+const score=100.7
+const scoreValue=100
+const isLoggedIn=true
+const outsideTemp=null;
+let userEmail;
+
+const id=Symbol('123');
+const anotherId=Symbol('123');
+
+// console.log(id===anotherId);
+
+const bigNumber=1234543242345n;
+
+// Reference (Non Premitive)
+
+// Array, Objects, Functions
+
+const heros=["Kantara","Garuda","Bhairava"];
+let myObj={
+    name:"Parth",
+    age:21
+}
+
+let myFunction=function(){
+    console.log("Hello,\n world!");
+}
+
+// myFunction();
+
+
