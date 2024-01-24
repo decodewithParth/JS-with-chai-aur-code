32dbcf3824a2874facc8dd4f970b938442b54364
decodewithParth <parthc2002@gmail.com>
2024-01-24 11:59:18 +0530

    Video - 5 : Conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score="Parth"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// true => 1 and false => 0
+
+
+let isLoggedIn=""
+
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1=> true ; 0 => false
+// "" => false
+// "Parth" =>true
+
+
+let someNumber=42
+
+let stringNumber=String(someNumber);
+console.log(stringNumber)
+console.log(typeof(stringNumber))
