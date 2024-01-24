63d031cd62bc11f095692454f21bd2325873f1d0
decodewithParth <parthc2002@gmail.com>
2024-01-24 10:35:00 +0530

    lerned about datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict";  // this will treat all JS code as newer version
+
+// alert(3+3)  // we are using NODE.JS not browser, so this will not work here directly
+
+console.log(3
+    +
+    3)  // code readability should be high
+
+console.log("Parth");
+
+let name="PArth"
+let age=21
+let isPass=false;
+
+
+// Data types in JS
+
+//  Premitive datatypes
+// Number => range 2 to power 53
+// bigint
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined
+// symbol => uniquiness 
+
+
+// object
+// console.log(typeof("Parth"));
+console.log(typeof(null));   // => object
+console.log(typeof(undefined));  // => undefined
