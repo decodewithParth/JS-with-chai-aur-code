8b5d74c606e22434cd2a09ff4d09335fe8306313
decodewithParth <parthc2002@gmail.com>
2024-01-24 15:48:46 +0530

    Video - 8 : Comparisons of datatypes in javascript

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,20 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);  // in this type of comparisions it will convert the string to numbers
+
+// console.log(null>0);
+// console.log(null==0);  // this happens because == and === works differntly than other comparisions operations 
+// console.log(null>=0);
+
+// console.log(undefined>0);
+// console.log(undefined==0);
+// console.log(undefined>=0);
+
+
+console.log("2"===2);
