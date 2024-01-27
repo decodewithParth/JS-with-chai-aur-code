16c880aa401a64055b8c3aa454225f966b699cea
decodewithParth <parthc2002@gmail.com>
2024-01-27 10:23:30 +0530

    Video - 17 : Objects part 2 in JS

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,59 @@
+// const tinderObj=new Object();
+const tinderUser={}
+
+tinderUser.id="#1001";
+tinderUser.name="Janak";
+tinderUser.isLoggedIn=false;
+
+// console.log(tinderUser);
+
+const regularUser={
+    email:"parth@google.com",
+    fullname:{
+        userfullname:{
+            firstname:"Parth",
+            middlename:"Dineshkumar",
+            lastname:"Chauhan"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.middlename);
+
+const obj1={1:"a",2:"b"};
+const obj2={3:"c",4:"d"};
+
+// const obj3=Object.assign(obj1,obj2); // in this static method of assign in objects the first parameter is called target and all other parameters are source which will going to merge
+// const obj3=Object.assign({},obj1,obj2);   // {} is target and all other are source
+const obj3={...obj1,...obj2}
+// console.log(obj3);
+
+
+const users=[
+    {
+        id:"1",
+        email:"1@gmail.com"
+    },
+    {
+        id:"2",
+        email:"2@gmail.com"
+    },
+    {
+        id:"3",
+        email:"3@gmail.com"
+    },
+    {
+        id:"4",
+        email:"4@gmail.com"
+    }
+]
+
+// console.log(users);
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('name'));
