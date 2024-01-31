8be47e2b9149dc26ed825d93808d08b5945ffefb
decodewithParth <parthc2002@gmail.com>
2024-01-31 16:55:03 +0530

    Video - 28 | While and do while loop in Javascript

---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+
+
+let index=0;
+while (index<=10) {
+    console.log(`value of index is ${index}`);
+    index++;
+}
+
+
+let heros=["flash","Spidy","hulk"];
+let size=0;
+
+while (size<heros.length) {
+    console.log(`Heros is ${heros[size]}`);
+    size++;
+}
+
+
+let score=1;
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score<11);
